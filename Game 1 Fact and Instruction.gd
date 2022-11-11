4e5565c4bb08c98fa63b2294f02b7485c597b375
marandabui <73601350+marandabui@users.noreply.github.com>
2022-11-11 18:38:36 -0500

    made game 1 fact scene and updated main menu

---
+++ Game 1 Fact and Instruction.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"VBoxContainer/start".grab_focus()
+
+func _on_start_pressed():
+	get_tree().change_scene("res://Scenes/Minigame_1/minigame_1.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
