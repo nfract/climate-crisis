085bde7a3e6ebabd49a080f18d048781704c2c4b
nfract <nfractt@gmail.com>
2022-12-05 19:32:32 -0500

    Added clickable trash and clickable oil rig.

--- Scenes/Minigame_2/Scripts/Tree.gd
+++ Scenes/Minigame_2/Scripts/Tree.gd
@@ -1,21 +1,12 @@
 extends Sprite
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	texture = load("res://Assets/Tree_Stump.png")
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
 
 func _on_WoodsButton_pressed():
 	texture = load("res://Assets/Tree1.png")
