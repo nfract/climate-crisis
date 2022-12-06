32072f53f939ae156070a34cf3191ecaec9e1f73
nfract <nfractt@gmail.com>
2022-12-06 16:13:13 -0500

    Added oilrig script.

---
+++ Scenes/Minigame_2/Scripts/Oilrig.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+func _ready():
+	texture = load("res://Assets/Oil_Pump.png")
+	pass 
+
+func _on_OilrigButton1_pressed():
+	texture = load("res://Assets/Nuclear_Reactor.png")
+	
+	# Used to ensure it is properly position to the original size of the oil rig and position
+	scale = Vector2(0.1, 0.1)
+	position = Vector2(position.x, position.y - 50)
+	pass
